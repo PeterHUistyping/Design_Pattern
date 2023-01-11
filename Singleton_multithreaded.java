f6944997b6c70a84b6e110081f0e1d97196345ed
PeterHUistyping <2447276617@qq.com>
2023-01-11 00:56:56 +0000

    updated code for singleton

--- Singleton_multithreaded.java
+++ Singleton_multithreaded.java
@@ -1,6 +1,6 @@
 public class Singleton_multithreaded {
     public static final class Singleton {
-        // The field must be declared volatile so that double check lock would work correctly.
+    // The field must be declared volatile so that double check lock would work correctly.
         private static volatile Singleton instance;
     
         public String value;
@@ -10,18 +10,18 @@ public class Singleton_multithreaded {
         }
     
         public static Singleton getInstance(String value) {
-            // The approach taken here is called double-checked locking (DCL). It
-            // exists to prevent race condition between multiple threads that may
-            // attempt to get singleton instance at the same time, creating separate
-            // instances as a result.
-            //
-            // It may seem that having the `result` variable here is completely
-            // pointless. There is, however, a very important caveat when
-            // implementing double-checked locking in Java, which is solved by
-            // introducing this local variable.
-            //
-            // You can read more info DCL issues in Java here:
-            // https://refactoring.guru/java-dcl-issue
+    // The approach taken here is called double-checked locking (DCL). It
+    // exists to prevent race condition between multiple threads that may
+    // attempt to get singleton instance at the same time, creating separate
+    // instances as a result.
+    //
+    // It may seem that having the `result` variable here is completely
+    // pointless. There is, however, a very important caveat when
+    // implementing double-checked locking in Java, which is solved by
+    // introducing this local variable.
+    //
+    // You can read more info DCL issues in Java here:
+    // https://refactoring.guru/java-dcl-issue
             Singleton result = instance;
             if (result != null) {
                 return result;
